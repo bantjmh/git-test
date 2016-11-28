d1d6160ac730c5298cce3d67bd350b260e48e826
chenbishuai <bantjmh@126.com>
2016-11-28 14:08:53 +0800

    modify Hello.java

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Helle ");
+        System.out.println("Hello ");
 		System.out.println("world");		
-		System.out.println("world");		
-		System.out.println("ni hao");		
-		System.out.println("ni hao");		
     }
 }
