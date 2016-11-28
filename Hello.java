14cf3792132a1eb7cbf68e24ff8186a797858ab1
chenbishuai <bantjmh@126.com>
2016-11-28 13:40:14 +0800

    update Hello

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Helle ");
 		System.out.println("world");		
-		System.out.println("ni hao");		
 		System.out.println("zhang san");		
     }
 }
--- README.md
+++ README.md
@@ -15,3 +15,6 @@
 	git reset HEAD Hello.java	//已添加&未提交
 	git reset --hard HEAD~1		//已添加&已提交  回退到上一个版本
 	git reset --hard 41dea12	//回退到指定版本
+
+###分支
+	
