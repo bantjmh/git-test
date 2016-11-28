61663ba7cb54e1fe5e603458caa9e10e41bbddaa
chenbishuai <bantjmh@126.com>
2016-11-28 14:20:19 +0800

    asdasdf

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ public class Hello{
         System.out.println("Hello ");
 		System.out.println("world");		
 		System.out.println("on branch master");
+		System.out.println("on branch master");
     }
 }
